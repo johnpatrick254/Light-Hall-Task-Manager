fa556162a60737b37e79f4392a268ced6083e092
John Patrick <jpattrick538@gmail.com>
2023-05-11 22:47:15 +0300

    disabled dotenv by lucas

--- BACKEND/config/config.php
+++ BACKEND/config/config.php
@@ -2,8 +2,8 @@
 declare(strict_types=1);
 require dirname(__DIR__) .'/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__) );
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__) );
+//$dotenv->load();
 
 define('HOSTNAME',$_ENV['HOSTNAME']);
 define('USERNAME',$_ENV['USERNAME']);
