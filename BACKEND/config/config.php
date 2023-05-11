a71f7f981467a3234c9b289bbe13abe3aa7d35c9
John Patrick <jpattrick538@gmail.com>
2023-05-11 21:56:41 +0300

    moved .env to root

--- BACKEND/config/config.php
+++ BACKEND/config/config.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 require dirname(__DIR__) .'/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__) );
 $dotenv->load();
 
 define('HOSTNAME',$_ENV['HOSTNAME']);
