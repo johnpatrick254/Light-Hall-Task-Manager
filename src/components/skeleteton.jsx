e6da28ab907a524831aca43c0df1142c48d66099
John Patrick <jpattrick538@gmail.com>
2023-05-12 23:37:33 +0300

    show one skeleton row when loading

--- src/components/skeleteton.jsx
+++ src/components/skeleteton.jsx
@@ -4,9 +4,6 @@ import LoadingBar from "./loading";
 const SkeleTon = () =>{
     return <div className="skeleton">
      <div> <LoadingBar height ="12rem" Width ="17.3rem"/></div>
-     <div> <LoadingBar height ="12rem" Width ="17.3rem"/></div>
-     <div> <LoadingBar height ="12rem" Width ="17.3rem"/></div>
-
       </div>
       
 
